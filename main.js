148401372e93149ea7fd221489e7e6954350a99d
Chike Ozulumba <o.devcode@gmail.com>
2023-06-21 00:44:30 +0100

    fix: refactoring

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
+// create constants to avoid repetitiveness
+const BG_SCROLL_CLASS = "bg-scroll";
+const IMDB_API_KEY = "19f84e11932abbc79e6d83f82d6d1045";
+
 // navbar scroll background
 const navBar = document.getElementById("nav");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    navBar.classList.add("bg-scroll");
-  } else {
-    navBar.classList.remove("bg-scroll");
-  }
-});
 // hero section background image and text fectch
 const headerBg = document.getElementById("header");
 const names = document.getElementById("name");
 const text = document.getElementById("overview");
+
+const originalsUrl = `https://api.themoviedb.org/3/discover/tv?api_key=${IMDB_API_KEY}&with_networks=213`;
+const trendingUrl = `https://api.themoviedb.org/3/trending/movie/week?api_key=${IMDB_API_KEY}`;
+const topRatedUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${IMDB_API_KEY}&language=en-US&page=1`;
+const comedyUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${IMDB_API_KEY}&with_genres=35`;
+
+const DB = [
+  {
+    path: `poster_path`,
+    element: ".original",
+    url: originalsUrl,
+  },
+  {
+    path: `poster_path`,
+    element: "#trending",
+    url: trendingUrl,
+  },
+  {
+    path: `poster_path`,
+    element: "#top-rated",
+    url: topRatedUrl,
+  },
+  {
+    path: `poster_path`,
+    element: "#comedy",
+    url: comedyUrl,
+  },
+];
+
 async function headerImage() {
   try {
     const response = await fetch(
-      "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
+      `https://api.themoviedb.org/3/discover/tv?api_key=${IMDB_API_KEY}&with_networks=213`
     );
     const data = await response.json();
 
@@ -29,18 +55,10 @@ async function headerImage() {
     displayError(error);
   }
 }
-headerImage();
+
 function displayError(imageerror) {
   console.log(imageerror.message);
 }
-
-// Call the main functions the page is loaded
-window.onload = () => {
-  getOriginals();
-  getTrendingNow();
-  getTopRated();
-  getComedies();
-};
 
 function fetchMovies(url, dom_element, path_type) {
   fetch(url)
@@ -60,7 +78,7 @@ function fetchMovies(url, dom_element, path_type) {
 }
 
 //  ** Function that displays the movies to the DOM **
-showMovies = (movies, dom_element, path_type) => {
+function showMovies(movies, dom_element, path_type) {
   // Create a variable that grabs id or class
   var moviesEl = document.querySelector(dom_element);
 
@@ -82,30 +100,29 @@ showMovies = (movies, dom_element, path_type) => {
     // Append the imageElement to the dom_element selected
     moviesEl.appendChild(imageElement);
   }
+}
+
+async function loadAllMovieData() {
+  try {
+    const payload = Object.values(DB).map(({ url, path, element }) =>
+      fetchMovies(url, element, path)
+    );
+    await Promise.all(payload);
+  } catch (error) {
+    displayError(error);
+  }
+}
+
+// Call the main functions the page is loaded
+window.onload = async () => {
+  await headerImage();
+  await loadAllMovieData();
 };
 
-// ** Function that fetches Netflix Originals **
-function getOriginals() {
-  let url =
-    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213";
-  fetchMovies(url, ".original", "poster_path");
-}
-// ** Function that fetches Trending Movies **
-function getTrendingNow() {
-  let url =
-    "https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045";
-  fetchMovies(url, "#trending", "poster_path");
-}
-// ** Function that fetches Top Rated Movies **
-function getTopRated() {
-  let url =
-    "https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1";
-  fetchMovies(url, "#top-rated", "poster_path");
-}
-
-function getComedies() {
-  let url =
-    "https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&with_genres=35";
-
-  fetchMovies(url, "#comedy", "poster_path");
-}
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navBar.classList.add(BG_SCROLL_CLASS);
+  } else {
+    navBar.classList.remove(BG_SCROLL_CLASS);
+  }
+});
